adfe93d5e8a7186222bfedc1b897d9e338db13d2
Subhadeep Mukherjee <subhadeepmukherjee2019@gmail.com>
2025-06-16 13:39:30 +0530

    First day practice

---
+++ app.js
@@ -0,0 +1,19 @@
+// window.console.log("Hello from js!");
+
+// let a= 22
+// const b = 2022
+// var age = 20
+
+// console.log(a,b,age)
+
+
+//variable is by default unfinded
+
+// console.log(age)
+// var age= 22   // declearartion and initialization   
+
+// var age = 20   //redeclearation
+ console.log(age)
+
+let age = 22
+console.log(age)
