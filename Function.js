97793b683f39ef66b008521cfbe541857c1c7f8d
Subhadeep Mukherjee <subhadeepmukherjee2019@gmail.com>
2025-06-17 09:59:09 +0530

    FUNCTION

---
+++ Function.js
@@ -0,0 +1,14 @@
+calculateJuiceBill(50,0.5);
+console.log("Start");
+function calculateJuiceBill(juice,gst){
+  //let gstOnJuice = 0.1;
+  let juicebill = juice+juice*gst;
+  console.log(juicebill);
+}
+//calculateJuiceBill();
+console.log("End");
+
+calculateJuiceBill(40,0.5);
+calculateJuiceBill(30,.05);
+
+u
